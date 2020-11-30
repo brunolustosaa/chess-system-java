273070fb8daba2dafe491df81dabc9cba5363172
brunolustosaa <brunohf123@outlook.com>
2020-11-30 16:03:40 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+	private static final long serialVersionUID = 1l;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
